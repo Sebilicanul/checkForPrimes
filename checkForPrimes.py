df5d24d05d4b0b2ad056be01cba2c7fecc1aa4f4
Sebastian Dimitriu <sebastiandimitriu@Sebastians-MacBook-Air.local>
2021-02-27 05:34:05 -0800

    urcare functie prime numbers

---
+++ checkForPrimes.py
@@ -0,0 +1,11 @@
+"""
+Function that checks if a certain number is prime or not
+"""
+def checkForPrimes(value):
+    for n in range(2,value):
+        if value%n == 0:
+            print(value,"is not a prime number. ",value,"is divisable by ",n)
+            break
+        elif n == value-1:
+            print(value,"is prime")     
+checkForPrimes(17)
